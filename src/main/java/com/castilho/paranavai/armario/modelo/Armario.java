0e51b88cae8fa08929d013a036804df491718e53
Wesley Castilho <wesleyguilhermecastilho@gmail.com>
2022-09-21 22:17:04 -0300

    Criado tabelas de Localização, armario e Pessoa com o hibernate

--- src/main/java/com/castilho/paranavai/armario/modelo/Armario.java
+++ src/main/java/com/castilho/paranavai/armario/modelo/Armario.java
@@ -4,19 +4,40 @@
  */
 package com.castilho.paranavai.armario.modelo;
 
+//Importações
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 /**
  *
  * @author Wesley
  */
+@Entity
+@Table(name = "tb_armario")
 public class Armario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id_armario", unique = true, nullable = false)
+    private Integer armarioId;
+
+    @Column(nullable = false)
     private String numero;
+
+    @Column(nullable = false)
     private boolean ativo;
+
+    @Column(nullable = false, length = 250)
     private String observacoes;
-    
-    public Armario(){
+
+    public Armario() {
     }
-    
-    public Armario(String numero, boolean ativo, String observacoes){
+
+    public Armario(String numero, boolean ativo, String observacoes) {
         this.numero = numero;
         this.ativo = ativo;
         this.observacoes = observacoes;
